a5ed502db3d0c4998b77bb8a2095a6f12652ff79
971023als <971023als@naver.com>
2023-07-28 20:44:17 +0900

    ok

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php    
-    $con = mysqli_connect("localhost", "user", "1q2w3e4r", "sample");
+    $con = mysqli_connect("localhost", "bobnuri", "nuribob1234!", "sample");
 
     mysqli_set_charset($con, "utf8");
 ?>
