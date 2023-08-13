e1dc0467b08a59a4742fafd2279e5acf4ff2da85
971023als <971023als@naver.com>
2023-08-14 01:02:50 +0900

    Update board_download.php

--- board_download.php
+++ board_download.php
@@ -15,10 +15,6 @@
          $file_name = iconv('utf-8', 'euc-kr', $file_name);
     }
 
-    // 로그인 상태가 아닌 경우, 본인 파일만 다운로드 가능
-    if(!isset($_SESSION['user_id']) && $_SESSION['user_id'] != $real_name) {
-        die('로그인 상태가 아니면 다운로드 불가합니다.');
-    }
 
     if( file_exists($file_path) )
     { 
