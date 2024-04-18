d0231d3f613ad1f70ec61c11fbaa858e8643b3f9
971023als <971023als@naver.com>
2024-04-19 08:07:24 +0900

    수정

--- pri.php
+++ pri.php
@@ -1,8 +1,7 @@
 <?php require('lib/top.php'); ?>
 <?php require('lib/copy.php'); ?> 
-<!-- 이 위치에 2023_07_17 파일의 내용이 삽입됩니다. -->
-<?php include('2023_07_17.html'); ?>
 <!-- 이 위치에 2024_04_19 파일의 내용이 삽입됩니다. -->
+<?php include('2024_04_19.html'); ?>
 <script src="js/jquery-2.1.3.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <?php require('lib/bottom.php'); ?>
